d18e640cc8b75a5c5ae6bcedbdbd02751797e334
Mitchell Hornsby <hornsby.mitchell@yahoo.com>
2024-10-29 19:52:35 -0700

    Added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
   return (
     <html lang="en" className='sm:!scroll-smooth'>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MFQP93LYRQ"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MFQP93LYRQ');
+          `}
+        </script>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>" />
       </head>
       <body className={`
