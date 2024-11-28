2faed698dedcf14c9b08a4b62680120dcd371218
Mitchell Hornsby <hornsby.mitchell@yahoo.com>
2024-11-27 19:27:11 -0800

    Reworked fe -> be data fetching

--- app/homePage/sections/Projects/components/ProjectTile.tsx
+++ app/homePage/sections/Projects/components/ProjectTile.tsx
@@ -1,32 +1,60 @@
-import { RiLoader3Fill } from "react-icons/ri";
+'use client'
 import ProjectTileDescription from "./ProjectTileDescription";
 import { Dayjs } from "@/app/services/dayjs";
 import { SocialLink } from "../../Landing";
-import { BsArrowUpRight, BsBoxArrowUpRight, BsGithub, BsLink } from "react-icons/bs";
-
+import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { RiLoader2Fill } from "react-icons/ri";
 
 type Props = {
     id: number
 }
 
+type Project = {
+    title: string
+    organization: {
+        name: string
+    }
+    skills: {
+        id: string
+        name: string
+        tagColor: string
+        tagFontColor: string
+    }[]
+    startDate: string
+    endDate: string
+    description_html: string
+    liveUrl: string
+    githubRepo: string
+}
 
-export default async function ProjectTile({ id }: Props) {
-    const project = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProject`,
+export default function ProjectTile({ id }: Props) {
+    const [loading, setLoading] = useState(true)
+    const [project, setProject] = useState<Project | undefined>()
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProject`,
+            {
+                method: 'POST',
+                body: JSON.stringify({ projectId: id })
+            }
+        )
+            .then(async (res) => {
+                const { data: { Project: project } } = await res.json()
+                setProject(project)
+                setLoading(false)
+            })
+    }, [])
+
+    return (<>
         {
-            method: 'POST',
-            body: JSON.stringify({ projectId: id })
-        }
-    )
-        .then(async (res) => {
-            const { data: { Project: project } } = await res.json()
-            return project
-        })
-
-    return (
-        <div className={`flex flex-col w-full text-lg font-primary drop-shadow-dark max-w-[1024px]`}>
-            {/* Project details */}
-            <div
-                className={`
+            loading
+                ? (<RiLoader2Fill className="animate-spin text-[4rem]" />)
+                : (project && (
+                    <div className={`flex flex-col w-full text-lg font-primary drop-shadow-dark max-w-[1024px]`}>
+                        {/* Project details */}
+                        <div
+                            className={`
                 flex
                 flex-1
                 flex-col
@@ -40,40 +68,40 @@ export default async function ProjectTile({ id }: Props) {
                 sm:py-8
                 sm:px-12
         `}>
-                <div className={`flex flex-row flex-wrap justify-between`}>
-                    <h3 className="flex flex-1 flex-row text-2xl font-header items-end">{project.title}</h3>
-                    <div className={`flex flex-row gap-4`}>
-                        {
-                            project.liveUrl && <SocialLink
-                                link={project.liveUrl}
-                                icon={<BsBoxArrowUpRight className="text-[1.5rem] !text-zinc-800 dark:!text-white" />}
-                                className={`!bg-white dark:!bg-zinc-800 !w-12 !h-12`}
-                            />
-                        }
-                        {
-                            project.githubRepo && <SocialLink
-                                link={project.githubRepo}
-                                icon={<BsGithub className="text-[2rem] !text-zinc-800 dark:!text-white" />}
-                                className={`!bg-white dark:!bg-zinc-800 !w-12 !h-12`}
-                            />
-                        }
-                    </div>
-                </div>
-                <div className={`flex`}>
-                    <p>{project.organization?.name}</p>
-                </div>
+                            <div className={`flex flex-row flex-wrap justify-between`}>
+                                <h3 className="flex flex-1 flex-row text-2xl font-header items-end">{project.title}</h3>
+                                <div className={`flex flex-row gap-4`}>
+                                    {
+                                        project.liveUrl && <SocialLink
+                                            link={project.liveUrl}
+                                            icon={<BsBoxArrowUpRight className="text-[1.5rem] !text-zinc-800 dark:!text-white" />}
+                                            className={`!bg-white dark:!bg-zinc-800 !w-12 !h-12`}
+                                        />
+                                    }
+                                    {
+                                        project.githubRepo && <SocialLink
+                                            link={project.githubRepo}
+                                            icon={<BsGithub className="text-[2rem] !text-zinc-800 dark:!text-white" />}
+                                            className={`!bg-white dark:!bg-zinc-800 !w-12 !h-12`}
+                                        />
+                                    }
+                                </div>
+                            </div>
+                            <div className={`flex`}>
+                                <p>{project.organization?.name}</p>
+                            </div>
 
-                <p className="flex italic">{`
+                            <p className="flex italic">{`
                     ${project.startDate ? Dayjs(project.startDate).format('MMM DD, YYYY') : ''}
                     ${project.startDate && project.endDate ? ' - ' : ''}
                     ${project.endDate ? Dayjs(project.endDate).format('MMM DD, YYYY') : ''}
                 `}</p>
-                <div className={`flex px-2 sm:px-4 py-2 sm:py-4`}>
-                    <ProjectTileDescription content={project.description_html} />
-                </div>
-            </div>
-            {/* Project skills */}
-            <div className={`
+                            <div className={`flex px-2 sm:px-4 py-2 sm:py-4`}>
+                                <ProjectTileDescription content={project.description_html} />
+                            </div>
+                        </div>
+                        {/* Project skills */}
+                        <div className={`
             relative
             px-2
             sm:px-4
@@ -83,9 +111,12 @@ export default async function ProjectTile({ id }: Props) {
             flex-wrap
             text-lg
             `}>
-                {project.skills.map((skill, i) => <SkillTag key={`${skill.id}`} {...skill} zIndex={i} />)}
-            </div>
-        </div>
+                            {project.skills.map((skill, i) => <SkillTag key={`${skill.id}`} {...skill} zIndex={i} />)}
+                        </div>
+                    </div>
+                ))
+        }
+    </>
     );
 }
 
--- app/homePage/sections/Projects/section.tsx
+++ app/homePage/sections/Projects/section.tsx
@@ -1,23 +1,36 @@
+'use client'
 import SectionContainer from "@/app/components/layouts/SectionContainer/SectionContainer";
 import ProjectTile from "./components/ProjectTile";
+import { useState, useEffect } from "react";
+import { RiLoader2Fill } from "react-icons/ri";
 
-export default async function Projects() {
-    const projects = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProjects`)
-        .then(async (res) => {
-            const { data: { Projects: { docs: projects } } } = await res.json()
-            return projects
-        })
+export default function Projects() {
+    const [loading, setLoading] = useState(true)
+    const [projects, setProjects] = useState<[{ id: number }] | undefined>()
 
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProjects`)
+            .then(async (res) => {
+                const { data: { Projects: { docs: projects } } } = await res.json()
+                setProjects(projects)
+                setLoading(false)
+            })
+    }, [])
+    
     return (
         <section id="projects" className="pt-32">
             <SectionContainer>
-                <div className="dm-text flex flex-col gap-8">
-                    <h2 className={`text-2xl font-header`}>Projects</h2>
-                    <div className="flex flex-col items-center gap-8">
-                        {projects.map((proj, i) => <ProjectTile key={`${proj.id}-${i}`} {...proj} />)}
-                    </div>
-                </div>
+                {loading
+                    ? (<RiLoader2Fill className="animate-spin text-[6rem]" />)
+                    : (projects && (<div className="dm-text flex flex-col gap-8">
+                        <h2 className={`text-2xl font-header`}>Projects</h2>
+                        <div className="flex flex-col items-center gap-8">
+                            {projects.map((proj, i) => <ProjectTile key={`${proj.id}-${i}`} {...proj} />)}
+                        </div>
+                    </div>))
+                }
             </SectionContainer>
         </section>
+
     );
 }
