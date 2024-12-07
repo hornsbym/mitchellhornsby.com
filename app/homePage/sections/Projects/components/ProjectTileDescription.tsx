d330135d09173387b1740c82b174e9084c36364c
Mitchell Hornsby <hornsby.mitchell@yahoo.com>
2024-12-06 16:01:37 -0800

    Added project tile description expand feature

--- app/homePage/sections/Projects/components/ProjectTileDescription.tsx
+++ app/homePage/sections/Projects/components/ProjectTileDescription.tsx
@@ -1,13 +1,35 @@
+import { ReactNode, useState } from "react";
 import "./ProjectTileDescriptionStyles.css"
+import { BsChevronBarContract, BsChevronBarExpand } from "react-icons/bs";
 
 type Props = {
     content: string
 }
 
 export default function ProjectTileDescription({ content }: Props) {
+    const [expanded, setIsExpanded] = useState(false)
+
     return (
-        <div className={`flex px-4 py-4`}>
-            <div id="description_content" dangerouslySetInnerHTML={{ __html: content }} />
+        <div className={`flex flex-col px-4 py-4 gap-4 items-center`}>
+            <div id="description_content" data-expanded={expanded} dangerouslySetInnerHTML={{ __html: content }} />
+            <ExpandToggle label={expanded 
+                ? <span className="flex flex-row gap-2 items-center">Hide details <BsChevronBarContract/></span> 
+                : <span className="flex flex-row gap-2 items-center">Expand details <BsChevronBarExpand/></span>} 
+                toggle={() => setIsExpanded(!expanded)} /> 
         </div>
     );
+}
+
+type ExpandToggleProps = {
+    toggle: () => void
+    label: ReactNode
+}
+
+const ExpandToggle = ({
+    toggle,
+    label
+}: ExpandToggleProps) => {
+    return (<button className="text-yellow-500 font-bold underline p-2 w-fit" onClick={toggle}>
+        {label}
+    </button>)
 }
